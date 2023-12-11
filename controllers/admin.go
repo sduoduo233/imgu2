104df1b42c37712919514b2b912c5142ef84e7db
duoduo <sduoduo233@gmail.com>
2023-12-11 09:53:47 +0800

    refactoring code

--- controllers/admin.go
+++ controllers/admin.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"imgu2/controllers/middleware"
-	"imgu2/services"
-	"log/slog"
 	"net/http"
 )
 
@@ -13,42 +11,4 @@ func adminIndex(w http.ResponseWriter, r *http.Request) {
 	render(w, "admin", H{
 		"user": user,
 	})
-}
-
-func adminSettings(w http.ResponseWriter, r *http.Request) {
-	user := middleware.MustGetUser(r.Context())
-
-	m, err := services.Setting.GetAll()
-	if err != nil {
-		slog.Error("admin settings", "err", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	render(w, "admin_settings", H{
-		"user":       user,
-		"setting":    m,
-		"csrf_token": csrfToken(w),
-	})
-}
-
-func doAdminSettings(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	form := r.Form
-	for k, v := range form {
-		err := services.Setting.Set(k, v[0])
-		if err != nil {
-			slog.Error("admin settings", "err", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	}
-
-	renderDialog(w, "Info", "Settings updated", "/admin/settings", "Continue")
 }
---
+++ controllers/admin_setting.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"imgu2/controllers/middleware"
+	"imgu2/services"
+	"log/slog"
+	"net/http"
+)
+
+func adminSettings(w http.ResponseWriter, r *http.Request) {
+	user := middleware.MustGetUser(r.Context())
+
+	m, err := services.Setting.GetAll()
+	if err != nil {
+		slog.Error("admin settings", "err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	render(w, "admin_settings", H{
+		"user":       user,
+		"setting":    m,
+		"csrf_token": csrfToken(w),
+	})
+}
+
+func doAdminSettings(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	form := r.Form
+	for k, v := range form {
+		err := services.Setting.Set(k, v[0])
+		if err != nil {
+			slog.Error("admin settings", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	renderDialog(w, "Info", "Settings updated", "/admin/settings", "Continue")
+}
